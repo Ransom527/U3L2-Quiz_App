f6a0ea59cad7b00b0683099b5357075aacc9bb78
Ransom527 <Ransom527@GMail.com>
2016-12-29 00:08:08 -0700

    v1.2

--- js/app.js
+++ js/app.js
@@ -2,7 +2,8 @@
 
 var numberCorrect = 0;
 var onQuestion = 0;
-var Questions =[ {
+var Questions =[
+{
 	Question:'What was the development codename for the Nintendo Gamecube?',
 	Choices:['Swordfish','Dolphin','Rhino','Panther'],
 	Answer:function(value){
@@ -27,6 +28,7 @@ var Questions =[ {
 		if(!value)
 			return 'Seattle Mariners'
 		return 'Seattle Mariners' == value;
+	}
 },
 {
 	Question:'Hyundai released the NES in South Korea under a different name due to import restrictions, what was it called?',
@@ -35,6 +37,7 @@ var Questions =[ {
 		if(!value)
 			return 'Comboy'
 		return 'Comboy' == value;
+	}
 },
 {
 	Question:'What was the name of the eagerly awaited add-on for the Nintedo 64, only realeased in Japan?',
@@ -43,6 +46,7 @@ var Questions =[ {
 		if(!value)
 			return 'N64 DD'
 		return 'N64 DD' == value;
+	}
 }
 ]
 
@@ -51,6 +55,18 @@ function gameStart() {
 	renderQuestion(Questions[onQuestion]);
 	$("#question-box").removeClass('hidden');
 	$("#start").addClass('hidden');
+}
+
+
+function newGame() {
+	onQuestion = 0;
+	numberCorrect = 0;
+	renderQuestion(Questions[onQuestion]);
+	$("#answer-box").text('');
+	$('.selections').prop('checked', false);
+	$("#question-box").removeClass('hidden');
+	$("#Submit").removeClass('hidden');
+	$("#new-game").addClass('hidden');
 }
 
 
@@ -64,17 +80,9 @@ function renderQuestion(data) {
 }
 
 
-function checkChecked(value) {
-	return document.getElementById(value).checked;
-}
-
-
 function onSubmit() {
 	event.preventDefault();
 	var choice
-	console.log('sumbit')
-	console.log(checkChecked('Option0'))
-	console.log(onQuestion, 'onQuestion #1')
 	if(checkChecked('Option0')){
 		choice = Questions[onQuestion].Choices[0];
 		console.log(choice)
@@ -104,40 +112,52 @@ function onSubmit() {
 	}
 	onQuestion += 1;
 	console.log(onQuestion, 'onQuestion')
-	$("#Submit").addClass('hidden');
-//	$("#next-question").removeClass('hidden');
 	console.log(numberCorrect, 'Number Correct')
-
+	$("#Submit").addClass('hidden');
 	if(onQuestion == Questions.length) {
-
+		gameOver();
+		$("#new-game").removeClass('hidden');
+		$("#question-box").addClass('hidden');
 		console.log('game over')
 	}
 	else {
 		$("#next-question").removeClass('hidden');
 	}
-	//if onQuestion value == (last question +1)
+}
 
+
+function checkChecked(value) {
+	return document.getElementById(value).checked;
+	//Returns selected answer
 }
 
 
 function nextQuestion() {
 	event.preventDefault();
-	//uncheck functoin
+	//uncheckAnswers();
+	//$('.selections').checked(false);
+	//$('.selections').checked = false;;
+	//document.GetElementByID(Option0).checked = false;
+	//document.GetElementByID(Option1).checked = false;
+	//document.GetElementByID(Option2).checked = false;
+	//document.GetElementByID(Option3).checked = false;
+	//$("input:radio").attr("checked", false);
+	//$(".selections").removeAttr("checked");
+	$('.selections').prop('checked', false);
 	$("#next-question").addClass('hidden');
 	$("#Submit").removeClass('hidden');
+	$("#answer-box").text('');
 	renderQuestion(Questions[onQuestion])
 }
 
-uncheckAnswers() {
+
+function gameOver() {
+	//define actions for 'game over'
+}
+
+
+function checkAnswer() {
 //
 }
 
-function checkAnswer() {
 
-}
-
-//add html element for numbercorrect and onquestion
-
-function newGame(){
-
-}
