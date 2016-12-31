bb1673faea60eb4058f1b8458e7e5c1b910bd31d
Ransom527 <Ransom527@GMail.com>
2016-12-31 14:00:52 -0700

    Before State Refactor

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 //JS Code
 
+//state management, use single object
+//state.Questions[i].Question
+//refactor Answer: 
 var numberCorrect = 0;
 var onQuestion = 0;
 var Questions =[
@@ -31,26 +34,48 @@ var Questions =[
 	}
 },
 {
-	Question:'Hyundai released the NES in South Korea under a different name due to import restrictions, what was it called?',
-	Choices:['Ultra 64','Comboy','iQue Player','Famicom'],
+	Question:'Hyundai released the Nintedo 64 in South Korea under a different name due to import restrictions, what was it called?',
+	Choices:['Ultra 64','Comboy 64','iQue Player','Famicom'],
 	Answer:function(value){
 		if(!value)
-			return 'Comboy'
-		return 'Comboy' == value;
+			return 'Comboy 64'
+		return 'Comboy 64' == value;
 	}
 },
 {
 	Question:'What was the name of the eagerly awaited add-on for the Nintedo 64, only realeased in Japan?',
-	Choices:['N64 DD','iQue Player','SatellaVision','Ultra 64'],
+	Choices:['N64 DD','iQue Player','Satellaview','Ultra 64'],
 	Answer:function(value){
 		if(!value)
 			return 'N64 DD'
 		return 'N64 DD' == value;
 	}
+},
+{
+	Question:'What was the name of the most expesive video game platform ever released?',
+	Choices:['3DO','Neo Geo AES','Phillips CD-i','Pioneer LaserActive'],
+	Answer:function(value){
+		if(!value)
+			return 'Pioneer LaserActive'
+		return 'Pioneer LaserActive' == value;
+	}
+},
+{
+	//updated
+	Question:'What was the name of the camera peripheral for the Sega Dreamcast, only released in Japan?',
+	Choices:['DreamSnap','SegaCam','Dreameye','WebCast'],
+	Answer: function(value){
+		if(!value) {
+			return 'Dreameye'
+		}
+		return 'Dreameye' == value;
+	}
 }
 ]
 
 
+
+//consolidate both
 function gameStart() {
 	renderQuestion(Questions[onQuestion]);
 	$("#question-box").removeClass('hidden');
@@ -69,7 +94,7 @@ function newGame() {
 	$("#new-game").addClass('hidden');
 }
 
-
+//loop through array and output answers
 function renderQuestion(data) {
 	console.log(data, 'data')
 	$('#Question').text(data.Question);
@@ -77,9 +102,11 @@ function renderQuestion(data) {
 	$('#Answer1').text(data.Choices[1]);
 	$('#Answer2').text(data.Choices[2]);
 	$('#Answer3').text(data.Choices[3]);
+	$('#on-question').text(onQuestion);
+	$('#number-correct').text(numberCorrect);
 }
 
-
+//refactor
 function onSubmit() {
 	event.preventDefault();
 	var choice
@@ -125,24 +152,17 @@ function onSubmit() {
 	}
 }
 
-
+//use jquery
 function checkChecked(value) {
 	return document.getElementById(value).checked;
+	//$("input:radio[name='r']:checked").val()
 	//Returns selected answer
 }
 
-
+//combine with submit, use alert box for answer
+//lookup function called timout for alert persistence
 function nextQuestion() {
 	event.preventDefault();
-	//uncheckAnswers();
-	//$('.selections').checked(false);
-	//$('.selections').checked = false;;
-	//document.GetElementByID(Option0).checked = false;
-	//document.GetElementByID(Option1).checked = false;
-	//document.GetElementByID(Option2).checked = false;
-	//document.GetElementByID(Option3).checked = false;
-	//$("input:radio").attr("checked", false);
-	//$(".selections").removeAttr("checked");
 	$('.selections').prop('checked', false);
 	$("#next-question").addClass('hidden');
 	$("#Submit").removeClass('hidden');
